0a35e23780397b495e18a29b0b21a0435a2ab33b
Vinnie Vendemia <vvendemia1@gmail.com>
2025-09-14 14:23:23 -0400

    Bump 0.1.3

--- Formula/divaide.rb
+++ Formula/divaide.rb
@@ -1,8 +1,8 @@
 class Divaide < Formula
   desc "Helper script to simplify launching multiple AI Agents"
   homepage "https://github.com/vinnievendemia/divaide"
-  url "https://github.com/vinnievendemia/divaide/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "7acb79fdb5d6701aa02d47fc91cfe84b53e6ee254817cc29ed07186a243002d0"
+  url "https://github.com/vinnievendemia/divaide/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "333926119151ff900aef4c0b680ad659fb39febb2cb985ed1ec7a4d27c7ecb01"
   license "MIT"
 
   def install
