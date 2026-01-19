ef2b1acc68ac155d3df8f78784ff7ab7c94bb1b4
Vinnie Vendemia <vvendemia1@gmail.com>
2026-01-19 15:37:44 -0500

    Bump v0.1.8

--- Formula/divaide.rb
+++ Formula/divaide.rb
@@ -1,8 +1,8 @@
 class Divaide < Formula
   desc "Helper script to simplify launching multiple AI Agents"
   homepage "https://github.com/vinnievendemia/divaide"
-  url "https://github.com/vinnievendemia/divaide/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "2316c7d8c31b95c619871d4150bbfc3b33fe0657501f575868b73aa58bbe77f9"
+  url "https://github.com/vinnievendemia/divaide/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "72fb2e0a402c7b17f8f8d685e6fa2b16dbf643005be10ea4b54d4013821a7189"
   license "MIT"
 
   def install
@@ -16,7 +16,7 @@ class Divaide < Formula
   def caveats
     <<~EOS
       To activate divaide, add the following to your shell config:
-        source #{etc}/divaide.sh
+        source "$(brew --prefix)/etc/divaide.sh"
     EOS
   end
 
